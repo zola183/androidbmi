87c71acb24edbb6d927d9ec25809bb5f60913bdf
gasolin <gasolin@14f6da24-c240-0410-806b-f117dd8960b1>
2007-12-08 09:55:30 +0000

    update bmi.java

--- bmi/src/com/demo/android/bmi/bmi.java
+++ bmi/src/com/demo/android/bmi/bmi.java
@@ -34,13 +34,14 @@ public class bmi extends Activity {
 
     		TextView result = (TextView)findViewById(R.id.result);
     		result.setText("Your BMI is "+nf.format(BMI));
+    		
     		//Give health advice
     		if(BMI>25){
     			showAlert("Warning", "You need go diet", "ok", false);
     		}else if(BMI<20){
     			showAlert("Warning", "You need eat more", "ok", false);
     		}else{
-    			showAlert("Congrat", "You are in good shape", "ok", false);
+    			showAlert("Congratulation", "You are in good shape", "ok", false);
     		}
     		
     		//Add error checking
