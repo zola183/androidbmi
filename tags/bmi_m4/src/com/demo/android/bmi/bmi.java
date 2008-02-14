492575238ed907932cc4bbfb5c04c332b2b66fba
gasolin <gasolin@14f6da24-c240-0410-806b-f117dd8960b1>
2008-02-14 16:38:26 +0000

    last version for sdk m4

---
+++ tags/bmi_m4/src/com/demo/android/bmi/bmi.java
@@ -0,0 +1,52 @@
+package com.demo.android.bmi;
+
+import java.text.DecimalFormat;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class bmi extends Activity {
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.main);
+        
+        //Listen for button clicks
+        Button button = (Button)findViewById(R.id.submit);
+        button.setOnClickListener(calcBMI);
+    }
+
+    private OnClickListener calcBMI = new OnClickListener()
+    {
+    	public void onClick(View v)
+    	{
+    		DecimalFormat nf = new DecimalFormat("0.0");
+    		EditText fieldheight = (EditText)findViewById(R.id.height);
+    		EditText fieldweight = (EditText)findViewById(R.id.weight);
+    		double height = Double.parseDouble(fieldheight.getText().toString())/100;
+    		double weight = Double.parseDouble(fieldweight.getText().toString());
+    		double BMI = weight / (height * height);
+
+    		TextView result = (TextView)findViewById(R.id.result);
+    		result.setText(getString(R.string.bmi_result) +nf.format(BMI));
+    		//Give health advice
+    		if(BMI>25){
+    			showAlert("Warning", getString(R.string.advice_heavy), "ok", false);
+    		}else if(BMI<20){
+    			showAlert("Warning", getString(R.string.advice_light), "ok", false);
+    		}else{
+    			showAlert("Congratulation", getString(R.string.advice_average), "ok", false);
+    		}
+    		
+    		//Add error checking
+    	}
+    };
+    
+}
