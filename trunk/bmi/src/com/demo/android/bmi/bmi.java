f8560bac3eb742e3fed4de23b074e200df0eef95
gasolin <gasolin@14f6da24-c240-0410-806b-f117dd8960b1>
2008-05-28 08:12:47 +0000

    add about menu

--- trunk/bmi/src/com/demo/android/bmi/bmi.java
+++ trunk/bmi/src/com/demo/android/bmi/bmi.java
@@ -3,7 +3,10 @@ package com.demo.android.bmi;
 import java.text.DecimalFormat;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -11,28 +14,34 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class bmi extends Activity {
-	
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.main);
-        
-        //Listen for button clicks
-        Button button = (Button)findViewById(R.id.submit);
-        button.setOnClickListener(calcBMI);
+        findViews();
+        setListensers(); 
     }
 
-    private EditText fieldheight;
+	private Button calcbutton;
+	private EditText fieldheight;
     private EditText fieldweight;
     private TextView result;
     private TextView fieldsuggest;
+
     private void findViews()
     {
+    	calcbutton = (Button)this.findViewById(R.id.submit);
     	fieldheight = (EditText)this.findViewById(R.id.height);
 		fieldweight = (EditText)this.findViewById(R.id.weight);
-		result = (TextView)findViewById(R.id.result);
-		fieldsuggest = (TextView)findViewById(R.id.suggest);
+		result = (TextView)this.findViewById(R.id.result);
+		fieldsuggest = (TextView)this.findViewById(R.id.suggest);
+    }
+    
+    //Listen for button clicks
+    private void setListensers() {
+    	calcbutton.setOnClickListener(calcBMI);
     }
     
     private OnClickListener calcBMI = new OnClickListener()
@@ -40,10 +49,10 @@ public class bmi extends Activity {
     	public void onClick(View v)
     	{
     		/*
-    		 * Main operations
+    		 * Main calc operations
     		 */
     		DecimalFormat nf = new DecimalFormat("0.0");
-    		findViews();
+    		
     		double height = Double.parseDouble(fieldheight.getText().toString())/100;
     		double weight = Double.parseDouble(fieldweight.getText().toString());
     		double BMI = weight / (height * height);
@@ -62,7 +71,36 @@ public class bmi extends Activity {
     		}
     		
     		//Add error checking
+    		//Intent i = new Intent(bmi.this, Report.class);
+    		//startActivity(i);
     	}
     };
-    
+
+	protected static final int MENU_OPTIONS_ID = Menu.FIRST;
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		menu.add(0, MENU_OPTIONS_ID, R.string.options_label,
+				new Runnable() {
+					public void run() {
+						openOptionsDialog();
+					}
+		});
+		return true;
+	}
+	
+	private void openOptionsDialog() {
+		View view = getViewInflate().inflate(R.layout.about, null, null);
+		new AlertDialog.Builder(this)
+		.setTitle(R.string.about_title)
+		.setView(view)
+		.setPositiveButton(R.string.ok_label,
+				new DialogInterface.OnClickListener(){
+					public void onClick(
+							DialogInterface dialoginterface, int i){
+					}
+				})
+		.show();
+	}
 }
