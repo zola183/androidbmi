ed4ced9ba53925cfbaaa267075e8b125c28d540d
gasolin <gasolin@14f6da24-c240-0410-806b-f117dd8960b1>
2008-06-03 05:23:06 +0000

    add Report activity

--- trunk/bmi/src/com/demo/android/bmi/R.java
+++ trunk/bmi/src/com/demo/android/bmi/R.java
@@ -16,10 +16,9 @@ public final class R {
     public static final class id {
         public static final int about_content=0x7f050000;
         public static final int height=0x7f050001;
-        public static final int report_content=0x7f050006;
-        public static final int result=0x7f050004;
+        public static final int report_content=0x7f050004;
+        public static final int report_suggest=0x7f050005;
         public static final int submit=0x7f050003;
-        public static final int suggest=0x7f050005;
         public static final int weight=0x7f050002;
     }
     public static final class layout {
@@ -28,8 +27,8 @@ public final class R {
         public static final int report=0x7f030002;
     }
     public static final class string {
-        public static final int about_msg=0x7f04000f;
-        public static final int about_title=0x7f04000e;
+        public static final int about_msg=0x7f04000e;
+        public static final int about_title=0x7f04000d;
         public static final int advice_average=0x7f040001;
         public static final int advice_average_tw=0x7f040004;
         public static final int advice_heavy=0x7f040002;
@@ -41,9 +40,8 @@ public final class R {
         public static final int bmi_btn=0x7f04000a;
         public static final int bmi_result=0x7f04000b;
         public static final int height=0x7f040008;
-        public static final int ok_label=0x7f040011;
-        public static final int options_label=0x7f040010;
-        public static final int report_msg=0x7f04000d;
+        public static final int ok_label=0x7f040010;
+        public static final int options_label=0x7f04000f;
         public static final int report_title=0x7f04000c;
         public static final int weight=0x7f040009;
     }
--- trunk/bmi/src/com/demo/android/bmi/bmi.java
+++ trunk/bmi/src/com/demo/android/bmi/bmi.java
@@ -1,17 +1,18 @@
 package com.demo.android.bmi;
 
-import java.text.DecimalFormat;
+//import java.text.DecimalFormat;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+//import android.widget.TextView;
 
 public class bmi extends Activity {
 
@@ -27,16 +28,12 @@ public class bmi extends Activity {
 	private Button calcbutton;
 	private EditText fieldheight;
     private EditText fieldweight;
-    private TextView result;
-    private TextView fieldsuggest;
 
     private void findViews()
     {
     	calcbutton = (Button)this.findViewById(R.id.submit);
     	fieldheight = (EditText)this.findViewById(R.id.height);
 		fieldweight = (EditText)this.findViewById(R.id.weight);
-		result = (TextView)this.findViewById(R.id.result);
-		fieldsuggest = (TextView)this.findViewById(R.id.suggest);
     }
     
     //Listen for button clicks
@@ -47,32 +44,14 @@ public class bmi extends Activity {
     private OnClickListener calcBMI = new OnClickListener()
     {
     	public void onClick(View v)
-    	{
-    		/*
-    		 * Main calc operations
-    		 */
-    		DecimalFormat nf = new DecimalFormat("0.0");
-    		
-    		double height = Double.parseDouble(fieldheight.getText().toString())/100;
-    		double weight = Double.parseDouble(fieldweight.getText().toString());
-    		double BMI = weight / (height * height);
-
-    		result.setText(getString(R.string.bmi_result) +nf.format(BMI));
-    		//Give health advice
-    		if(BMI>25){
-    			//showAlert("Warning", getString(R.string.advice_heavy), "ok", false);
-    			fieldsuggest.setText(R.string.advice_heavy_tw);
-    		}else if(BMI<20){
-     			//showAlert("Warning", getString(R.string.advice_light), "ok", false);
-    			fieldsuggest.setText(R.string.advice_light_tw);
-    		}else{
-    			//showAlert("Congratulation", getString(R.string.advice_average), "ok", false);
-    			fieldsuggest.setText(R.string.advice_average_tw);
-    		}
-    		
-    		//Add error checking
-    		//Intent i = new Intent(bmi.this, Report.class);
-    		//startActivity(i);
+    	{	
+    		//Switch to report page
+    		Intent i = new Intent(bmi.this, Report.class);
+    		Bundle b = new Bundle(); 
+    		b.putString("KEY_HEIGHT", fieldheight.getText().toString());
+    		b.putString("KEY_WEIGHT", fieldweight.getText().toString());
+    		i.putExtras(b);
+    		startActivity(i);
     	}
     };
 
