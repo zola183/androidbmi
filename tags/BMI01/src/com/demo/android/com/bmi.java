fd9a29c393e0b4d98fa259f2fdac536d3d7f2121
gasolin <gasolin@14f6da24-c240-0410-806b-f117dd8960b1>
2008-07-25 17:09:07 +0000

    tagging BMI01

---
+++ tags/BMI01/src/com/demo/android/com/bmi.java
@@ -0,0 +1,13 @@
+package com.demo.android.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class bmi extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.main);
+    }
+}
