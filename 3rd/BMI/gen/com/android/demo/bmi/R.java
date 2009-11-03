1e4cbddfc611930f16654cecabe114428749712e
gasolin <gasolin@14f6da24-c240-0410-806b-f117dd8960b1>
2009-11-03 12:06:13 +0000

    fit ch9 sample

--- 3rd/BMI/gen/com/android/demo/bmi/R.java
+++ 3rd/BMI/gen/com/android/demo/bmi/R.java
@@ -24,7 +24,13 @@ public final class R {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int advice_average=0x7f040001;
+        public static final int advice_heavy=0x7f040002;
+        public static final int advice_light=0x7f040000;
+        public static final int app_name=0x7f040003;
+        public static final int bmi_btn=0x7f040006;
+        public static final int bmi_result=0x7f040007;
+        public static final int height=0x7f040004;
+        public static final int weight=0x7f040005;
     }
 }
