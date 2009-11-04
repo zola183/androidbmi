df5e8edc623f83aa91da285d93442d20a7ce395c
gasolin <gasolin@14f6da24-c240-0410-806b-f117dd8960b1>
2009-11-04 05:14:16 +0000

    example for dialog and toast

--- trunk/3rd/BMI/src/com/demo/android/bmi/Bmi.java
+++ trunk/3rd/BMI/src/com/demo/android/bmi/Bmi.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Bmi extends Activity {
     /** Called when the activity is first created. */
@@ -41,7 +42,7 @@ public class Bmi extends Activity {
     private Button.OnClickListener calcBMI = new Button.OnClickListener() { 
         public void onClick(View v) {
             DecimalFormat nf = new DecimalFormat("0.00");
-            
+            try {
             double height = Double.parseDouble(fieldheight.getText().toString())/100;
             double weight = Double.parseDouble(fieldweight.getText().toString());
             double BMI = weight / (height * height);
@@ -57,6 +58,33 @@ public class Bmi extends Activity {
             } else {
             	view_suggest.setText(R.string.advice_average);
             }
+            openOptionsDialog();
+            }catch(Exception obj) {
+            	  Toast.makeText(Bmi.this, R.string.input_error, Toast.LENGTH_SHORT).show();
+            }
         }
     };
+    
+//    public void openOptionsDialog(View target){
+//    	new AlertDialog.Builder(Bmi.this)
+//        .setTitle("關於 Android BMI")
+//        .setMessage("Android BMI Calc")
+//        .show();
+//    }
+    
+    private void openOptionsDialog() {
+//    	Toast.makeText(Bmi.this, "BMI 計算器", Toast.LENGTH_SHORT).show();
+    	
+//        new AlertDialog.Builder(Bmi.this)
+//        .setTitle(R.string.about_title)
+//        .setMessage(R.string.about_msg)
+//        .setPositiveButton("確認",
+//        new DialogInterface.OnClickListener(){
+//            public void onClick(
+//                DialogInterface dialoginterface, int i){
+//            }
+//        })
+//        .show();
+    }
+    
 }
