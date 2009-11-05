e342b58dc00dab57470089574c1e63e2d0423f36
gasolin <gasolin@14f6da24-c240-0410-806b-f117dd8960b1>
2009-11-05 03:25:15 +0000

    bundle example

--- trunk/3rd/BMI/gen/com/demo/android/bmi/R.java
+++ trunk/3rd/BMI/gen/com/demo/android/bmi/R.java
@@ -17,6 +17,7 @@ public final class R {
     }
     public static final class id {
         public static final int height=0x7f050000;
+        public static final int report_back=0x7f050005;
         public static final int result=0x7f050003;
         public static final int submit=0x7f050002;
         public static final int suggest=0x7f050004;
@@ -27,19 +28,20 @@ public final class R {
         public static final int report=0x7f030001;
     }
     public static final class string {
-        public static final int about_msg=0x7f04000a;
-        public static final int about_title=0x7f040009;
+        public static final int about_msg=0x7f04000b;
+        public static final int about_title=0x7f04000a;
         public static final int advice_average=0x7f040001;
         public static final int advice_heavy=0x7f040002;
         public static final int advice_light=0x7f040000;
-        public static final int app_name=0x7f040004;
-        public static final int bmi_btn=0x7f040007;
-        public static final int bmi_height=0x7f040005;
-        public static final int bmi_result=0x7f040008;
-        public static final int bmi_weight=0x7f040006;
-        public static final int homepage_label=0x7f04000d;
-        public static final int input_error=0x7f04000b;
-        public static final int ok_label=0x7f04000c;
+        public static final int app_name=0x7f040005;
+        public static final int bmi_btn=0x7f040008;
+        public static final int bmi_height=0x7f040006;
+        public static final int bmi_result=0x7f040009;
+        public static final int bmi_weight=0x7f040007;
+        public static final int homepage_label=0x7f04000e;
+        public static final int input_error=0x7f04000c;
+        public static final int ok_label=0x7f04000d;
+        public static final int report_back=0x7f040004;
         public static final int report_title=0x7f040003;
     }
 }
