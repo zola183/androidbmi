62c9527e9f40027e727d43adafac53b73fcf70d0
gasolin <gasolin@14f6da24-c240-0410-806b-f117dd8960b1>
2009-11-05 09:42:52 +0000

    add tw locale

--- trunk/3rd/aBMI/gen/com/demo/android/abmi/R.java
+++ trunk/3rd/aBMI/gen/com/demo/android/abmi/R.java
@@ -8,18 +8,22 @@
 package com.demo.android.abmi;
 
 public final class R {
+    public static final class array {
+        public static final int feets=0x7f050000;
+        public static final int inches=0x7f050001;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int feet=0x7f050000;
-        public static final int inch=0x7f050001;
-        public static final int result=0x7f050004;
-        public static final int submit=0x7f050003;
-        public static final int suggest=0x7f050005;
-        public static final int weight=0x7f050002;
+        public static final int feet=0x7f060000;
+        public static final int inch=0x7f060001;
+        public static final int result=0x7f060004;
+        public static final int submit=0x7f060003;
+        public static final int suggest=0x7f060005;
+        public static final int weight=0x7f060002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
