699777a9e0f78eae9f2b11ece32c364d197fbd35
gasolin <gasolin@14f6da24-c240-0410-806b-f117dd8960b1>
2009-11-06 03:29:47 +0000

    add spinner

--- 3rd/aBMI/src/com/demo/android/abmi/Abmi.java
+++ 3rd/aBMI/src/com/demo/android/abmi/Abmi.java
@@ -10,9 +10,11 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -38,15 +40,17 @@ public class Abmi extends Activity {
         findViews();
         setListensers();
         
-        ArrayAdapter<String> adapter_feet = new ArrayAdapter<String>(this,
-        	    android.R.layout.simple_spinner_item,
-        	    feets);
-        	adapter_feet.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        ArrayAdapter<String> adapter_feet = new ArrayAdapter<String>(this,
+//        	    android.R.layout.simple_spinner_item,
+//        	    feets);
+//        adapter_feet.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);    	
     }
 
     private Button button_calc;
-    private EditText field_feet;
-    private EditText field_inch;
+//    private EditText field_feet;
+//    private EditText field_inch;
+    private Spinner field_feet;
+    private Spinner field_inch;
     private EditText field_weight;
     private TextView view_result;
     private TextView view_suggest;
@@ -54,8 +58,10 @@ public class Abmi extends Activity {
     private void findViews() {
         Log.d(TAG, "find Views");
         button_calc = (Button) findViewById(R.id.submit);
-        field_feet = (EditText) findViewById(R.id.feet);
-        field_inch = (EditText) findViewById(R.id.inch);
+//        field_feet = (EditText) findViewById(R.id.feet);
+//        field_inch = (EditText) findViewById(R.id.inch);
+        field_feet = (Spinner) findViewById(R.id.feet);
+        field_inch = (Spinner) findViewById(R.id.inch);
         field_weight = (EditText) findViewById(R.id.weight);
         view_result = (TextView) findViewById(R.id.result);
         view_suggest = (TextView) findViewById(R.id.suggest);
@@ -65,24 +71,48 @@ public class Abmi extends Activity {
                 android.R.layout.simple_spinner_item);
         adapter_feet.setDropDownViewResource(
                 android.R.layout.simple_spinner_dropdown_item);
-
+        field_feet.setAdapter(adapter_feet);
         ArrayAdapter<CharSequence> adapter_inch = ArrayAdapter.createFromResource(
                 this, R.array.inches, 
-                android.R.layout.simple_spinner_item);
+                android.R.layout.simple_spinner_dropdown_item);
         adapter_inch.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        field_inch.setAdapter(adapter_inch);
     }
 
+    private int feet;
+    private int inch;
+    
+    private Spinner.OnItemSelectedListener getFeet = new Spinner.OnItemSelectedListener() {
+        public void onItemSelected(AdapterView parent, View v, int position, long id) {
+                feet = parent.getSelectedItemPosition()+2;
+        }
+        public void onNothingSelected(AdapterView parent) {
+        }
+    };
+
+    private Spinner.OnItemSelectedListener getInch = new Spinner.OnItemSelectedListener() {
+        public void onItemSelected(AdapterView parent, View v, int position, long id) {
+                inch = parent.getSelectedItemPosition()+1;
+        }
+        public void onNothingSelected(AdapterView parent) {
+
+        }
+    };
+    
     //Listen for button clicks
     private void setListensers() {
         Log.d(TAG, "set Listensers");
         button_calc.setOnClickListener(calcUsBMI);
+        field_feet.setOnItemSelectedListener(getFeet);
+        field_inch.setOnItemSelectedListener(getInch);
     }
 
     private Button.OnClickListener calcUsBMI = new Button.OnClickListener() {
         public void onClick(View v) {
             DecimalFormat nf = new DecimalFormat("0.00");
             try {
-                double height = (Double.parseDouble(field_feet.getText().toString())*12+Double.parseDouble(field_inch.getText().toString()))*2.54/100;
+//                double height = (Double.parseDouble(field_feet.getText().toString())*12+Double.parseDouble(field_inch.getText().toString()))*2.54/100;
+            	double height = (feet*12+inch)*2.54/100;
                 double weight = Double.parseDouble(field_weight.getText().toString())*0.45359;
                 double BMI = weight / (height * height);
                 //Present result
