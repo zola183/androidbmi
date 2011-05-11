0ec9552e8548ff1bb1eac851cfe7f3789420b8b1
gasolin <gasolin@14f6da24-c240-0410-806b-f117dd8960b1>
2011-05-11 09:07:09 +0000

    update

--- trunk/4th/CH35_DummyNote/src/com/demo/android/dummynote/Main.java
+++ trunk/4th/CH35_DummyNote/src/com/demo/android/dummynote/Main.java
@@ -2,9 +2,11 @@ package com.demo.android.dummynote;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
+import android.widget.SimpleCursorAdapter;
 
 public class Main extends ListActivity {
     /** Called when the activity is first created. */
@@ -26,13 +28,40 @@ public class Main extends ListActivity {
             */
     };
     
+    private DB mDbHelper;
+    private Cursor mNotesCursor;
+
     private void setAdapter() {
 //        ListAdapter adapter = new ArrayAdapter<String>(this,
 //                    android.R.layout.simple_list_item_1,
 //                    note_array);
 //        setListAdapter(adapter);
-        setListAdapter(new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
-                note_array));
+
+//        setListAdapter(new ArrayAdapter<String>(this,
+//                android.R.layout.simple_list_item_1,
+//                note_array));
+    	
+    	mDbHelper = new DB(this);
+        mDbHelper.open();
+        
+//        ListAdapter adapter = new ArrayAdapter<String>(this,
+//                android.R.layout.simple_list_item_1,
+//                note_array);
+//        setListAdapter(adapter);
+        
+        fillData();
+    }
+    
+    private void fillData() {
+        mNotesCursor = mDbHelper.getAll();
+        startManagingCursor(mNotesCursor);
+
+        String[] from = new String[]{"note"};
+        int[] to = new int[]{android.R.id.text1};
+
+        // Now create a simple cursor adapter
+        SimpleCursorAdapter adapter =
+                    new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, mNotesCursor, from, to);
+        setListAdapter(adapter);
     }
 }
