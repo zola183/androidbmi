66d4bbd2fa3f1da03ff81290161054efe9527ebd
gasolin <gasolin@14f6da24-c240-0410-806b-f117dd8960b1>
2011-05-15 08:17:36 +0000

    add menu declare in xml

--- 4th/CH15_BMI/gen/com/demo/android/bmi/R.java
+++ 4th/CH15_BMI/gen/com/demo/android/bmi/R.java
@@ -16,14 +16,19 @@ public final class R {
         public static final int icon=0x7f020002;
     }
     public static final class id {
-        public static final int height=0x7f050000;
-        public static final int result=0x7f050003;
-        public static final int submit=0x7f050002;
-        public static final int suggest=0x7f050004;
-        public static final int weight=0x7f050001;
+        public static final int about=0x7f060005;
+        public static final int finish=0x7f060006;
+        public static final int height=0x7f060000;
+        public static final int result=0x7f060003;
+        public static final int submit=0x7f060002;
+        public static final int suggest=0x7f060004;
+        public static final int weight=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+    }
+    public static final class menu {
+        public static final int menu=0x7f050000;
     }
     public static final class string {
         public static final int about_msg=0x7f040009;
